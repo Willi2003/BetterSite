5429ba817a706b283943b8098199540aa1c9493b
RomainLAU <romain.laurent@epitech.digital>
2022-04-28 12:09:13 +0200

    changing from red background to opacity background

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ let buttons = document.querySelectorAll('body>.Buttons>button')
 setInterval(() => {
     randomButton = buttons[Math.floor(Math.random() * 1000)]
 
-    randomButton.style.background = 'red'
+    randomButton.style.backgroundColor = 'rgba(0, 0, 0, 0.15)'
 }, 1000)
 
 let clickMe = document.querySelector('#click')
 
 alert('WOW GG !')
+
+document.addEventListener('contextmenu', (event) => {
+    event.preventDefault()
+})
