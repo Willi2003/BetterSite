9da7a2041633dd6d3359def5e3ef3ea6ffa7d969
Willi2003 <93141936+Willi2003@users.noreply.github.com>
2022-04-28 14:42:21 +0200

    update security

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-let buttons = document.querySelectorAll('body>.Buttons>button')
-let clickMe = document.querySelector('#click')
-let glass = document.querySelector("#glass")
+document.addEventListener('DOMContentLoaded', () => {
+    let buttons = document.querySelectorAll('body>.Buttons>button')
+    let clickMe = document.querySelector('#click')
+    let glass = document.querySelector("#glass")
 
-setInterval(() => {
-    randomButton = buttons[Math.floor(Math.random() * 1000)]
+    setInterval(() => {
+        randomButton = buttons[Math.floor(Math.random() * 1000)]
 
-    randomButton.style.background = 'red'
-}, 1000)
+        randomButton.style.background = 'red'
+    }, 1000)
 
-clickMe.addEventListener("click", () => {
-    alert('WOW GG !')
+    clickMe.addEventListener("click", () => {
+        alert('WOW GG !')
+    })
+
+    setInterval(() => {
+        glass = document.querySelector("#glass")
+        if (typeof(glass) == 'undefined' || glass == null) {
+            let newDiv = document.createElement("div")
+            newDiv.setAttribute('id', 'glass')
+            document.body.insertBefore(newDiv, clickMe)
+        } else if (glass.style.display == "none") {
+            glass.style.display = "block"
+        } else if (glass.style.position != "absolute") {
+            glass.style.position = "absolute"
+        } else if (glass.style.marginTop > "0") {
+            glass.style.marginTop = "0"
+        } else if (glass.style.marginBottom > "0") {
+            glass.style.marginBottom = "0"
+        } else if (glass.style.marginLeft > "0") {
+            glass.style.marginLeft = "0"
+        } else if (glass.style.marginRight > "0") {
+            glass.style.marginRight = "0"
+        }
+    }, 100)
 })
-
-setInterval(() => {
-    glass = document.querySelector("#glass")
-    if (typeof(glass) == 'undefined' || glass == null) {
-        let newDiv = document.createElement("div")
-        newDiv.setAttribute('id', 'glass')
-        document.body.insertBefore(newDiv, clickMe)
-    } else if (glass.style.display == "none") {
-        glass.style.display = "block"
-    } else if (glass.style.position != "absolute") {
-        glass.style.position = "absolute"
-    } else if (glass.style.marginTop > "0") {
-        glass.style.marginTop = "0"
-    }
-}, 100)
