252a9386ae04a742a6ee8bd915af24a60251b3c1
Willi2003 <93141936+Willi2003@users.noreply.github.com>
2022-04-28 13:05:27 +0200

    update glass security

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let buttons = document.querySelectorAll('body>.Buttons>button')
+let clickMe = document.querySelector('#click')
+let glass = document.querySelector("#glass")
 
 setInterval(() => {
     randomButton = buttons[Math.floor(Math.random() * 1000)]
@@ -6,8 +8,21 @@ setInterval(() => {
     randomButton.style.background = 'red'
 }, 1000)
 
-let clickMe = document.querySelector('#click')
-
 clickMe.addEventListener("click", () => {
     alert('WOW GG !')
 })
+
+setInterval(() => {
+    glass = document.querySelector("#glass")
+    if (typeof(glass) == 'undefined' || glass == null) {
+        let newDiv = document.createElement("div")
+        newDiv.setAttribute('id', 'glass')
+        document.body.insertBefore(newDiv, clickMe)
+    } else if (glass.style.display == "none") {
+        glass.style.display = "block"
+    } else if (glass.style.position != "absolute") {
+        glass.style.position = "absolute"
+    } else if (glass.style.marginTop > "0") {
+        glass.style.marginTop = "0"
+    }
+}, 100)
