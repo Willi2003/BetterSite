4d6e494de0a0d3aeef8ed6a91389a172506ee372
RomainLAU <romain.laurent@epitech.digital>
2022-04-28 11:17:35 +0200

    modifying something

--- index.js
+++
@@ -1,7 +0,0 @@
-let buttons = document.querySelectorAll('body>button')
-
-setInterval(() => {
-    randomButton = buttons[Math.floor(Math.random() * 1000)]
-
-    randomButton.style.backgroundColor = 'red'
-}, 1000)
---
+++ script.js
@@ -0,0 +1,11 @@
+let buttons = document.querySelectorAll('body>button')
+
+setInterval(() => {
+    randomButton = buttons[Math.floor(Math.random() * 1000)]
+
+    randomButton.style.backgroundColor = 'red'
+}, 1000)
+
+let clickMe = document.querySelector('#click')
+
+alert('WOW GG !')
