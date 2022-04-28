6091421abe715365f5afd0ffd3a6a80c2b4bdd2e
RomainLAU <romain.laurent@epitech.digital>
2022-04-28 16:58:32 +0200

    finishing project

--- script2.js
+++ script2.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    let enterRange = document.querySelector('#enter')
+    let enterValue = document.querySelector('#enterValue')
+
+    let randomValue = Math.floor(Math.random() * 100)
+
+    enterRange.addEventListener('input', () => {
+        if (enterRange.value == randomValue) {
+            alert('You are registered !')
+        }
+
+        enterValue.innerHTML = enterRange.value
+    })
+
+    let title = document.querySelector('h1')
+
+    title.addEventListener('click', () => {
+        title.style.fontSize = "25em"
+    })
 })
